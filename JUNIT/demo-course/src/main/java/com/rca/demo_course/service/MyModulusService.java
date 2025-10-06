178fc520edbb2217d498e7360891dd4897cebcde
Mchrispin <mchrispin14@gmail.com>
2025-10-06 13:52:30 +0200

    deleted JUNIT/demo_course

--- JUNIT/demo-course/src/main/java/com/rca/demo_course/service/MyModulusService.java
+++
@@ -1,8 +0,0 @@
-package com.rca.demo_course.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface MyModulusService {
-    double modulus(double dividend, double divisor);
-}
